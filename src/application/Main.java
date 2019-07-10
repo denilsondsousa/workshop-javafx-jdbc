b26c1bcac3f9c4a260251cb649a31b99ea524a1f
denilsondsousa <denilsondssufpi@gmail.com>
2019-07-10 09:43:31 -0300

    Main view controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);			
 			scrollPane.setFitToWidth(true);
 			
-			primaryStage.setScene(mainScene);
+			primaryStage.setScene(mainScene);	
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
 		} catch(IOException e) {
